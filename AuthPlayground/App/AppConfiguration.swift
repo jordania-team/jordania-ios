588956b6d86d02c3b4d4af3eb031f14fda381702
ghabrielferrari <ghcferrari@gmail.com>
2026-06-17 09:19:25 -0300

    chore(config): point to AWS and update bundle ID to rodrigoborges.TarefasPOC
    - AppConfiguration: remove DEBUG/Release split, single AWS URL
    - Info.plist: remove NSAllowsLocalNetworking (HTTPS only)
    - Bundle ID: com.ferrari.AuthPlayground → rodrigoborges.TarefasPOC

--- AuthPlayground/App/AppConfiguration.swift
+++ AuthPlayground/App/AppConfiguration.swift
@@ -8,11 +8,6 @@
 import Foundation
 
 /// Centraliza as configurações de ambiente da aplicação.
-/// Em Debug aponta para o backend local, em Release para produção.
 enum AppConfiguration {
-    #if DEBUG
-    static let apiBaseURL = URL(string: "http://10.40.57.27:8080")!
-    #else
     static let apiBaseURL = URL(string: "https://api.redepets.xyz")!
-    #endif
 }
